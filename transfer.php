f6f7504c4adf1824450cc1b3f619e429497a8252
Burning-Hell-Fire <60735305+Burning-Hell-Fire@users.noreply.github.com>
2021-08-25 11:02:00 +0530

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -85,7 +85,7 @@ if(isset($_POST['submit']))
 <!-- Navigation bar-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Hero Bank  </a>
+    <a class="navbar-brand" href="main.php">Hero Bank  </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
